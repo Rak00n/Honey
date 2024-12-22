8fb18503d1f1caa14c35b8f1edc01900425803fb
Rak00n <>
2024-12-22 20:29:15 +0300

    linux+

---
+++ getInterface.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net"
+	"os"
+	"os/exec"
+	"runtime"
+	"strings"
+)
+
+func checkIPAddress(ip string) bool {
+	if net.ParseIP(ip) == nil {
+		return false
+	} else {
+		return true
+	}
+}
+
+func getInterfaceAndIPs(macAddress string) (string, []string) {
+	detectedDevice := ""
+	detectedIPs := []string{}
+	if runtime.GOOS == "windows" {
+		//macAddressUC := strings.ToUpper(strings.ReplaceAll(macAddress, "-", ":"))
+		macAddressUH := strings.ToUpper(strings.ReplaceAll(macAddress, ":", "-"))
+		macAddressLC := strings.ToLower(strings.ReplaceAll(macAddress, "-", ":"))
+		//macAddressLH := strings.ToLower(strings.ReplaceAll(macAddress, ":", "-"))
+		result, err := exec.Command("cmd", "/C", "getmac").Output()
+		if err != nil {
+			fmt.Println("Something went wrong: ", err)
+			os.Exit(1)
+		} else {
+			resultSlice := strings.Split(string(result), "\r\n")
+			for _, v := range resultSlice {
+				//fmt.Println(v)
+				if strings.Contains(v, macAddressUH) {
+					v = strings.ReplaceAll(v, " ", "")
+					detectedDevice = strings.ReplaceAll(v, macAddressUH, "")
+					detectedDevice = strings.ReplaceAll(detectedDevice, "Tcpip_{", "NPF_{")
+					break
+				}
+			}
+		}
+		interfaces, _ := net.Interfaces()
+		for _, v := range interfaces {
+			testMAC := v.HardwareAddr.String()
+			if testMAC == macAddressLC {
+				ips, _ := v.Addrs()
+				for _, ip := range ips {
+					ipSlice := strings.Split(ip.String(), "/")
+					tempIP := ipSlice[0]
+					detectedIPs = append(detectedIPs, tempIP)
+				}
+			}
+		}
+		//os.Exit(1)
+	} else {
+		macAddressLC := strings.ToLower(strings.ReplaceAll(macAddress, "-", ":"))
+		files, err := ioutil.ReadDir("/sys/class/net/")
+		if err != nil {
+			fmt.Println("Something went wrong: ", err)
+			os.Exit(1)
+		}
+
+		for _, file := range files {
+			//fmt.Println(file.Name())
+			//fmt.Println(macAddressLC)
+			dat, _ := os.ReadFile("/sys/class/net/" + file.Name() + "/address")
+			if strings.Contains(string(dat), macAddressLC) {
+				detectedDevice = file.Name()
+				break
+			}
+			//fmt.Print(string(dat))
+
+		}
+		result, err := exec.Command("/bin/sh", "-c", "hostname --all-ip-addresses").Output()
+		if err != nil {
+			fmt.Println("Something went wrong: ", err)
+			os.Exit(1)
+		} else {
+			resultSlice := strings.Split(string(result), "\n")
+			for _, v := range resultSlice {
+				vTemp := strings.Trim(v, " ")
+				if checkIPAddress(vTemp) {
+					detectedIPs = append(detectedIPs, vTemp)
+				}
+			}
+		}
+	}
+	//fmt.Println(detectedIPs)
+	return detectedDevice, detectedIPs
+}
--- main.go
+++ main.go
@@ -2,43 +2,46 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"net"
-	"os"
-	"strings"
-
 	"github.com/google/gopacket"
 	"github.com/google/gopacket/layers"
 	"github.com/google/gopacket/pcap"
+	"log"
+	"os"
+	"strings"
 )
 
-func main() {
-	interfaceToListen := "E0-2E-0B-31-B1-8B"
-	portsToListen := []int{80, 443, 22}
-	interfaceToListen = strings.ReplaceAll(interfaceToListen, "-", ":")
-	interfaceToListen = strings.ToLower(interfaceToListen)
-	// Open the device for capturing
-	fmt.Println("Listing all network interfaces...")
-	ifaces, err := net.Interfaces()
-	if err == nil {
-		for _, iface := range ifaces {
-			fmt.Println(iface.HardwareAddr.String())
-			if iface.HardwareAddr.String() == interfaceToListen {
-				fmt.Println("Found interface", iface.Name)
-			}
+func portInList(port string, listOfPorts []string) bool {
+	for _, portItem := range listOfPorts {
+		if portItem == port {
+			return true
 		}
 	}
-	fmt.Println(portsToListen)
-	os.Exit(1)
-	devs, err := pcap.FindAllDevs()
-	for _, dev := range devs {
-		fmt.Println(dev.Name)
-		fmt.Println(dev.Addresses)
-		fmt.Println(dev.Description)
-	}
-	//fmt.Println(devs)
+	return false
+}
 
-	handle, err := pcap.OpenLive("\\Device\\NPF_{F727CFFF-1919-434B-A395-F7CA0A2954AD}", 1600, true, pcap.BlockForever)
+func addressInList(ip string, listOfIPs []string) bool {
+	for _, ipItem := range listOfIPs {
+		if ipItem == ip {
+			return true
+		}
+	}
+	return false
+}
+
+func main() {
+	//interfaceMAC := "E0:2E:0B:31:B1:8B"
+	interfaceMAC := "00:15:5D:4F:2E:7A"
+	portsToListen := []string{"80", "443", "22"}
+
+	deviceToCapture, ipsToCapture := getInterfaceAndIPs(interfaceMAC)
+	if deviceToCapture == "" {
+		fmt.Println("No device to capture")
+		os.Exit(1)
+	}
+	fmt.Println("Device MAC: " + interfaceMAC)
+	fmt.Println("Device IPs: " + strings.Join(ipsToCapture, " "))
+	fmt.Println("Honeypot launched on " + deviceToCapture)
+	handle, err := pcap.OpenLive(deviceToCapture, 1600, true, pcap.BlockForever)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -50,23 +53,20 @@ func main() {
 		log.Fatal(err)
 	}
 
-	// Use the handle as a packet source to process all packets
 	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
 	for packet := range packetSource.Packets() {
-		// Process packet here
-		// Check for the TCP layer
-		tcpLayer := packet.Layer(layers.LayerTypeTCP)
-		if tcpLayer != nil {
-			tcp, _ := tcpLayer.(*layers.TCP)
-
-			// Print TCP information
-			log.Printf("From src port: %d to dst port: %d\n", tcp.SrcPort, tcp.DstPort)
-			log.Printf("Sequence number: %d\n", tcp.Seq)
-
-			// If there's payload, print it as a string
-			if len(tcp.Payload) > 0 {
-				log.Printf("Payload: %s\n", string(tcp.Payload))
+		networkLevel := packet.NetworkLayer().NetworkFlow()
+		if addressInList(networkLevel.Dst().String(), ipsToCapture) {
+			tcpLayer := packet.Layer(layers.LayerTypeTCP)
+			if tcpLayer != nil {
+				tcp, _ := tcpLayer.(*layers.TCP)
+				dstPortSlice := strings.Split(tcp.DstPort.String(), "(")
+				dstPort := dstPortSlice[0]
+				if portInList(dstPort, portsToListen) {
+					fmt.Println("Alert! Packet to: ", dstPort)
+				}
 			}
 		}
+
 	}
 }
